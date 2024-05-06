aac446d93d3050b5ab8404b08e423f75e5b282bd
omarotierra <2omarhernandez2@gmail.com>
2024-05-05 23:20:22 -0600

    first commit

---
+++ src/main/java/org/uv/DAPP01Practica03/Dapp01Practica03Application.java
@@ -0,0 +1,13 @@
+package org.uv.DAPP01Practica03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dapp01Practica03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dapp01Practica03Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/DAPP01Practica03/Dapp01Practica03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.DAPP01Practica03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dapp01Practica03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
